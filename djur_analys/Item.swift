fe4abab26e191abffb683166ef97fb34434d5fcb
Johnny Slätt <johnny.slatt@gmail.com>
2023-11-02 14:08:09 +0100

    Initial Commit

---
+++ djur_analys/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  djur_analys
+//
+//  Created by Johnny Slätt on 2023-11-02.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
